13fb61ba441a20438efbf1646aa03261c448ef07
turim0 <ivan_medvedev_1996@mail.ru>
2019-02-22 12:21:25 +0300

    simplified the code

--- duplicates.py
+++ duplicates.py
@@ -1,32 +1,38 @@
 from collections import defaultdict
 import os
 import sys
+import argparse
 
 
-def put_files_to_storage(folder_path, storage):
+def put_files_to_storage(folder_path):
+    storage = defaultdict(list)
     for directory, subdirectories, files in os.walk(folder_path):
         for filename in files:
             full_path = os.path.join(directory, filename)
             file_size = os.path.getsize(full_path)
             storage[filename, file_size].append(full_path)
+    return storage
 
 
-def get_dublicates(storage):
-    keys_with_dublicates = filter(lambda x: len(storage[x]) > 1, storage.keys())
-    dublicates_info = list(map(lambda x: (x, storage[x]), keys_with_dublicates))
-    return dublicates_info
+def get_duplicates(storage):
+    duplicates_info = {file_info: file_paths
+                       for file_info, file_paths in storage.items() if len(file_paths)>1
+                       }
+    return duplicates_info
+
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print('Usage: {0} + folder path'.format(sys.argv[0]))
-        sys.exit()
-    folder_path = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('folder_path', help='input folder path')
+    arg = parser.parse_args()
+    folder_path = arg.folder_path
     if not os.path.isdir(folder_path):
         sys.exit('Please pass correct directory name')
-    storage = defaultdict(list)
-    put_files_to_storage(folder_path, storage)
-    dublicates_info = get_dublicates(storage)
-    if not dublicates_info:
-        sys.exit('No dublicates founded')
-    for file_info, file_path in dublicates_info:
-        print('Dublicates {0} founded at {1}'.format(file_info[0], file_path))
+    storage = put_files_to_storage(folder_path)
+    duplicates_info = get_duplicates(storage)
+    if not duplicates_info:
+        print("No dublicates founded")
+        sys.exit()
+    for file_info, file_paths in duplicates_info.items():
+        print('Founded dublicates {0}: {1} bytes'.format(*file_info))
+        print("\n".join(file_paths))
